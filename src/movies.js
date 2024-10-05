55679393d29928e132655ff560b3fdb0e990ee35
Airam Santos diaz <airam@macbook-pro-de-airam.home>
2024-10-05 14:52:15 +0200

    iteration3 done

--- src/movies.js
+++ src/movies.js
@@ -1,6 +1,33 @@
+/*const movies = [
+    {
+      title: 'The Shawshank Redemption',
+      year: 1994,
+      director: 'Frank Darabont',
+      duration: '2h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9.3
+    },
+    {
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Ford Coppola',
+      duration: '2h 55min',
+      genre: ['Crime', 'Drama'],
+      score: 9.2
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      director: 'Francis Ford Coppola',
+      duration: '3h 22min',
+      genre: ['Crime', 'Drama'],
+      score: 9
+    }];*/
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
+
 function getAllDirectors(moviesArray) {
 	const directors = moviesArray.map((movie) => {
 		return movie.director;
@@ -19,8 +46,19 @@ function howManyMovies(moviesArray) {
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if(moviesArray.length === 0){
+        return 0;
+    }
 
+    const totalScore = moviesArray.reduce((acc, curr) =>{
+        return acc = curr.score
+    }, 0);
+    const scoresAverage = totalScore / moviesArray.length
+    const scoresAverageTwoDecimals = +scoresAverage.toFixed(2);
+    return scoresAverageTwoDecimals; //no encontramos la solucion 
+}
+    console.log(scoresAverage(movies));
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
 
